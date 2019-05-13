951d8888f1cadb74943e2d34913e0371c4dd7e82
Michael Baynon <baynonmichael@gmail.com>
2019-05-13 18:28:05 -0400

    WiP

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,9 +1,14 @@
+'use strict';
+
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
 // let myFunction = function () {
 // console.log("Function was invoked!");
 // };
 // myFunction();
+
+
+
 
 // let anotherFunction = function (param) {
 //   return param;
